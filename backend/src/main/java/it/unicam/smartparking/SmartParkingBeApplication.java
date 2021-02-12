831cabd7695066fae7f224b08998da6226c10dff
Uran Tamnik <uran.tamnik@studenti.unicam.it>
2021-02-12 02:03:25 +0200

    Beginning

---
+++ backend/src/main/java/it/unicam/smartparking/SmartParkingBeApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.smartparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartParkingBeApplication.class, args);
+	}
+
+}
